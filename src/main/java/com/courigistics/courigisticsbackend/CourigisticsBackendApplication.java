74b5662b687e716ea55250ffc4cbeec6a92482d2
Salano <xirlanostephen@gmail.com>
2025-09-23 17:01:25 +0300

    Initial Spring Boot backend project setup

---
+++ src/main/java/com/courigistics/courigisticsbackend/CourigisticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.courigistics.courigisticsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourigisticsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourigisticsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/courigistics/courigisticsbackend/CourigisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courigistics.courigisticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourigisticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
