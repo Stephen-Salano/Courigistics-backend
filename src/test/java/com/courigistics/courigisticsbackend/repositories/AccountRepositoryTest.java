09a0b26e71d536f1e04ffa72cc878e44c1ceef37
Salano <xirlanostephen@gmail.com>
2025-11-29 16:25:46 +0300

    refactor(test): extract account setup to @BeforeEach in AccountRepositoryTest
    Setup account, customer, and address once in @BeforeEach instead of duplicating in each test. This reduces code duplication and makes tests easier to maintain.

--- src/test/java/com/courigistics/courigisticsbackend/repositories/AccountRepositoryTest.java
+++ src/test/java/com/courigistics/courigisticsbackend/repositories/AccountRepositoryTest.java
@@ -5,6 +5,8 @@ import com.courigistics.courigisticsbackend.entities.Address;
 import com.courigistics.courigisticsbackend.entities.Customer;
 import com.courigistics.courigisticsbackend.entities.enums.AccountType;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -27,11 +29,15 @@ public class AccountRepositoryTest {
 
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    @Test
-    public void findByUsername_givenExitingUsername_thenReturnAccount(){
+    private Account testAccount;
+    private Customer testCustomer;
+    private Address testAddress;
 
+
+    @BeforeEach
+    public void setUpAccountEntity(){
         // Arrange
-        Address homeAddress = Address.builder()
+         testAddress = Address.builder()
                 .label("home")
                 .addressLine1("123 Avery Lane")
                 .addressLine2("1234 Avery Lane 2")
@@ -44,13 +50,13 @@ public class AccountRepositoryTest {
                 .createdAt(LocalDateTime.now())
                 .build();
 
-        Customer salano = Customer.builder()
+        testCustomer = Customer.builder()
                 .firstName("Stephen")
                 .lastName("Salano")
                 .profileImageUrl("/url/profile")
                 .build();
 
-        Account newAccount = Account.builder()
+        testAccount = Account.builder()
                 .username("salano")
                 .email("salano@gmail.com")
                 .phone("0712345689")
@@ -61,13 +67,19 @@ public class AccountRepositoryTest {
                 .build();
 
         // Act
-        homeAddress.setAccount(newAccount);
-        newAccount.setAddresses(List.of(homeAddress));
-        salano.setAccount(newAccount);
-        newAccount.setCustomer(salano);
+       testAddress.setAccount(testAccount);
+       testAccount.setAddresses(List.of(testAddress));
+       testCustomer.setAccount(testAccount);
+       testAccount.setCustomer(testCustomer);
 
-        accountRepository.save(newAccount);
-        Optional<Account> user = accountRepository.findByUsername(newAccount.getUsername());
+        accountRepository.save(testAccount);
+
+    }
+
+    @Test
+    @DisplayName("Should return an account given the existing username")
+    public void findByUsername_givenExitingUsername_thenReturnAccount(){
+        Optional<Account> user = accountRepository.findByUsername(testAccount.getUsername());
 
         // Assert
         Assertions.assertThat(user).isPresent();
@@ -76,53 +88,14 @@ public class AccountRepositoryTest {
     }
 
     @Test
+    @DisplayName("should return an account given an email")
     public void findByEmail_givenExistingEmail_thenReturnAccount(){
-        // arrange
-        // Arrange
-        Address homeAddress = Address.builder()
-                .label("home")
-                .addressLine1("123 Avery Lane")
-                .addressLine2("1234 Avery Lane 2")
-                .city("Nairobi")
-                .postalCode("36463-0100")
-                .country("Kenya")
-                .latitude(4.732849)
-                .longitude(87.391527)
-                .isDefault(true)
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        Customer salano = Customer.builder()
-                .firstName("Stephen")
-                .lastName("Salano")
-                .profileImageUrl("/url/profile")
-                .build();
-
-        Account newAccount = Account.builder()
-                .username("salano")
-                .email("salano@gmail.com")
-                .phone("0712345689")
-                .password(passwordEncoder.encode("Microsoft2554$"))
-                .accountType(AccountType.CUSTOMER)
-                .enabled(true)
-                .emailVerified(true)
-                .build();
-
-        // Act
-        homeAddress.setAccount(newAccount);
-        newAccount.setAddresses(List.of(homeAddress));
-        salano.setAccount(newAccount);
-        newAccount.setCustomer(salano);
-
-        accountRepository.save(newAccount);
-
-        // Act
-
-        Optional<Account> user = accountRepository.findByEmail(newAccount.getEmail());
+        Optional<Account> user = accountRepository.findByEmail(testAccount.getEmail());
         // assert
 
         Assertions.assertThat(user).isPresent(); // is present
         Assertions.assertThat(user.get().getEmail()).isNotNull(); // is not null
-        Assertions.assertThat(user.get().getEmail()).isEqualTo(newAccount.getEmail()); // is the right email
+        Assertions.assertThat(user.get().getEmail()).isEqualTo(testAccount.getEmail()); // is the right email
     }
+
 }
